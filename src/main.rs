e9472eb836d98c400b80523ee4145ccd807e6dbd
Shahiryar Saleem <shahiryarsaleem@Shahiryars-MacBook-Pro.local>
2024-01-02 11:12:18 +0500

    Add concept of vairables and mutability

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 fn main() {
     // Variables by default are immutable
     // using mut keyword a variable can be made mutable
+    let y = 12;
+    //y is an immutabel variable; no new value can be bound to it
     let mut x = 10;
     println!("The value of x is {x}");
     x = 5;
