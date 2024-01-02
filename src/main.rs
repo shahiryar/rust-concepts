13ebbab7abff355ac55244b057693c028adbd849
shahiryar <shahiryarsaleem@gmail.com>
2024-01-02 22:23:45 +0500

    Add shadow concept

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-const YEAR_OF_INDEPENDENCE:u32 = 1947;
+//const YEAR_OF_INDEPENDENCE:u32 = 1947;
 fn main() {
 //------- VARIABLES & CONSTANTS --------------
     // Variables by default are immutable
@@ -9,9 +9,17 @@ fn main() {
     //println!("The value of x is {x}");
     //x = 5;
     //println!("The value of x is {x}");
-    println!("The Year Pakistan was born on is {YEAR_OF_INDEPENDENCE}");
+    //println!("The Year Pakistan was born on is {YEAR_OF_INDEPENDENCE}");
 //----------------------------------------------
 //------- SHADOWING  ---------------------------
 //----------------------------------------------
-    //
+    //Creating a immutable variable
+   let x = 10;
+   println!("value of x in main scope before it is overshadowed : {x}");
+   let x = x+1; //over-shadowing variable
+   {
+       let x = 23;
+       println!("Shadowed x in local scope {x}");
+    }
+   println!("out of scope shadowed {x}");
 }
