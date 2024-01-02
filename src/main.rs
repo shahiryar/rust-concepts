7c770f4f6a1dcbf69fa428ab739889004e40beae
Shahiryar Saleem <shahiryarsaleem@Shahiryars-MacBook-Pro.local>
2024-01-02 11:21:39 +0500

    Add concept of Constants

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
+const YEAR_OF_INDEPENDENCE:u32 = 1947;
 fn main() {
+//------- VARIABLES & CONSTANTS --------------
     // Variables by default are immutable
     // using mut keyword a variable can be made mutable
-    let y = 12;
+    //let y = 12;
     //y is an immutabel variable; no new value can be bound to it
-    let mut x = 10;
-    println!("The value of x is {x}");
-    x = 5;
-    println!("The value of x is {x}");
+    //let mut x = 10;
+    //println!("The value of x is {x}");
+    //x = 5;
+    //println!("The value of x is {x}");
+    println!("The Year Pakistan was born on is {YEAR_OF_INDEPENDENCE}");
+//----------------------------------------------
+//------- SHADOWING  ---------------------------
+//----------------------------------------------
+    //
 }
