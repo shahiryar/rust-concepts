f52de13f615dcdedbc59b227274898e2d807d1cb
Shahiryar Saleem <shahiryarsaleem@Shahiryars-MacBook-Pro.local>
2024-01-02 11:09:46 +0500

    Add Initial code

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    // Variables by default are immutable
+    // using mut keyword a variable can be made mutable
+    let mut x = 10;
+    println!("The value of x is {x}");
+    x = 5;
+    println!("The value of x is {x}");
+}
