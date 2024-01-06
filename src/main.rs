efe62f63f3a4d2704468bcc4d29095c1b5aafbfe
shahiryar <shahiryarsaleem@gmail.com>
2024-01-06 21:29:29 +0500

    Add Compound Variable

--- src/main.rs
+++ src/main.rs
@@ -32,28 +32,35 @@ fn main() {
     //Interger Types
     //Integer could be unsigned (staring with u) or signed (starting with i)
     //u8, u16, u32, u64, u128, usize
-    let x:u32 = 123;
+    //let x:u32 = 123;
     //i8, i16, i32, i64, i128, isize
-    let y:i32 = -23;
-    println!("{x}, {y}");
+    //let y:i32 = -23;
+    //println!("{x}, {y}");
     
     //Floating point number
-    let z: f32 = 2.3;
-    println!("{z}");
+    //let z: f32 = 2.3;
+    //println!("{z}");
 
     //NUMERIC OPERATIONS
-    let sum = 3+2;
-    let difference = 2-1;
-    let product = 8*3;
-    let quotient  = 56.7/32.2;
-    let truncated = -5/3;
-    let remainder = 3%2;
+    //let sum = 3+2;
+    //let difference = 2-1;
+    //let product = 8*3;
+    //let quotient  = 56.7/32.2;
+    //let truncated = -5/3;
+    //let remainder = 3%2;
 
-    println!("{sum}");
-    println!("{difference}");
-    println!("{product}");
-    println!("{quotient}");
-    println!("{truncated}");
-    println!("{remainder}");
+    //println!("{sum}");
+    //println!("{difference}");
+    //println!("{product}");
+    //println!("{quotient}");
+    //println!("{truncated}");
+    //println!("{remainder}");
 
+//------COMPOUND DATATYPES-------------------
+    //Tuples
+    let tup:(u8, i8, i8, i32) = (0, -2, -1, -432);
+    let zero = tup.0;
+    let minus_two = tup.1;
+    println!("{zero}");
+    println!("{minus_two}");
 }
